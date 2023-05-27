403f73990f1a0a5140057cbb9acad414a304946f
Arnulfo Acosta <arnulfoacosta0887@gmail.com>
2023-05-27 12:33:33 -0500

    update logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
             <div class="row">
 
                 <div class="col-2 pt-2">
-                    <img src=" {{ asset('./img/logo2.png') }} " alt="" width="100">
+                    <img src=" {{ asset('./img/logo2.png') }} " alt="" width="120">
 
                 </div>
                 <div class="col-10 ">
@@ -76,7 +76,7 @@
 
 
 
-        <div class="row mt-3">
+        <div class="row ">
             <div class="col-12 rounded  px-0">
                 <div class="">
                     @if(isset($newsDesktop) && $newsDesktop->items->count() >0)
