28527fd4bce1e140d033aabc9b48fea17134bf38
Arnulfo Acosta <arnulfoacosta0887@gmail.com>
2023-09-05 22:20:27 -0500

    se cambia nombre de membresías para logo

--- resources/views/livewire/account-show-order.blade.php
+++ resources/views/livewire/account-show-order.blade.php
@@ -281,12 +281,12 @@ $preference->save();
                                                     </small>
                                                     <br>
 
-                                                    @if($membership->title=='Preescolar VIP 2023-2024')
+                                                    @if($membership->title=='Preescolar 2023-2024')
                                                     <a href="https://api.whatsapp.com/send?phone=+9981838908&text=Quiero%20activar%20mi%20membres%C3%ADa%20PREESCOLAR%20-%20compra%20web: {{ $order->id }} - {{auth()->user()->email}} " target="_blank">
                                                     <img src="{{ asset('img/whatsapp1.png') }}" alt="logo WhatsApp" width="60">
                                                     </a>
 
-                                                    @elseif($membership->title=='Primaria VIP 2023-2024')
+                                                    @elseif($membership->title=='Primaria 2023-2024')
 
                                                     <a href="https://api.whatsapp.com/send?phone=+9981838908&text=Quiero%20activar%20mi%20membres%C3%ADa%20PRIMARIA%20-%20compra%20web: {{ $order->id }} - {{auth()->user()->email}} " target="_blank">
                                                     <img src="{{ asset('img/whatsapp1.png') }}" alt="logo WhatsApp" width="60">
