19289005edcd4eb806e452b222a4a1da63635014
Arnulfo Acosta <arnulfoacosta0887@gmail.com>
2023-10-12 20:05:36 -0500

    se elimina trait

--- app/User.php
+++ app/User.php
@@ -19,18 +19,20 @@ namespace App;
 
 
 
+
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Mchev\Banhammer\Traits\Bannable;
+
+
 
 
 class User extends Authenticatable
 {
     use Notifiable;
     use HasRoles;
-    use Bannable;
+  
  
  
     
